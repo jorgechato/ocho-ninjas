659206f7e2c8b8979ab8c9c105a2af653bdb29ee
jorgechato <hack@jorgechato.com>
2023-04-17 14:47:36 +0900

    ADD TEST DB

--- ocho/settings.py
+++ ocho/settings.py
@@ -87,7 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'TEST': {
+            'NAME': BASE_DIR / 'db-test.sqlite3',
+        },
+    },
 }
 
 HEALTH_CHECK = {
